50039510dee69cee19509152db93183d0291ebf5
Mundus <zS22017046@estudiantes.uv.mx>
2025-10-16 13:52:26 -0600

    add: login scene

--- scenes/login/login.gd
+++ scenes/login/login.gd
@@ -1,20 +1,21 @@
 extends Control
 
 func _ready():
-	$HTTPRequest.request_completed.connect(_http_request_completed)
-	var data = {
-		"name": "Hector",
-		"email": "godot@gmail.com",
-		"password": "godot1234"
-	}
-	
-	var body = JSON.new().stringify(data)
-	var headers = ["Content-Type: application/json"]
-	var url = "https://fracto-api.onrender.com/api/v1/auth/register"
-	
-	var error = $HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, body)
-	if error != OK:
-		push_error("An error occurred in the HTTP request.")
+	pass
+	#$HTTPRequest.request_completed.connect(_http_request_completed)
+	#var data = {
+		#"name": "Hector",
+		#"email": "godot@gmail.com",
+		#"password": "godot1234"
+	#}
+	#
+	#var body = JSON.new().stringify(data)
+	#var headers = ["Content-Type: application/json"]
+	#var url = "https://fracto-api.onrender.com/api/v1/auth/register"
+	#
+	#var error = $HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, body)
+	#if error != OK:
+		#push_error("An error occurred in the HTTP request.")
 
 
 func _http_request_completed(result, response_code, headers, body):
