e04cc13f3f27dcdd1d249805126591ac25a7d839
Julian <zS22017046@estudiantes.uv.mx>
2025-10-24 08:17:30 -0600

    add: login and register no response/network error handling

--- scenes/login/login.gd
+++ scenes/login/login.gd
@@ -2,44 +2,44 @@ extends Control
 
 @onready var email_field = $PanelContainer/VBoxContainer2/Form/Fields/Email
 @onready var password_field = $PanelContainer/VBoxContainer2/Form/Fields/Password
-
 @onready var login_button = $PanelContainer/VBoxContainer2/Form/Buttons/VBoxContainer/Login
 @onready var register_button = $PanelContainer/VBoxContainer2/Form/Buttons/HBoxContainer/Register
-
 @onready var login_error_label = $PanelContainer/VBoxContainer2/Form/Fields/ErrorLabel
+
+const ERRORS_RESPONSE = "errors"
+const MESSAGE_RESPONSE = "message"
 
 var email = ""
 var password = ""
-
-const ERRORS_RESPONSE = "errors"
-
 var error_theme = null
 var normal_theme = null
+
 
 func _ready():
 	error_theme = load("res://scenes/register/line_edit_error.tres")
 	normal_theme = load("res://scenes/register/line_edit_normal.tres")
 
+
 func _on_register_pressed():
 	SceneManager.change_scene(SceneManager.SCENES.REGISTER)
+
 
 func _on_login_pressed():
 	if _is_data_invalid():
 		return
 	_login()
-	
+
+
 func _is_data_invalid():
-	# get data from fields
 	email = email_field.text.strip_edges()
 	password = password_field.text.strip_edges()
 	
-	# check if empty
 	var is_email_empty = InputValidator.is_input_empty(email, email_field)
 	var is_password_empty = InputValidator.is_input_empty(password, password_field)
 	
-	# return boolean
 	return is_email_empty or is_password_empty
-	
+
+
 func _login():
 	login_button.text = "Iniciando sesión..."
 	login_button.disabled = true
@@ -52,7 +52,6 @@ func _login():
 		"email": email,
 		"password": password,
 	}
-	
 	var body = JSON.stringify(data)
 	var headers = ["Content-Type: application/json", "Accept: application/json"]
 	var url = "https://fracto-api.onrender.com/api/v1/auth/login"
@@ -61,20 +60,27 @@ func _login():
 	if response != OK:
 		login_error_label.text = "Error de conexión"
 		push_error("An error occurred in the HTTP request.")
-	
+
 
 func _http_request_completed(result, response_code, headers, body):
 	var json = JSON.new()
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
+	print(response_code, response)
+	
+	if response:
+		login_error_label.visible = response.has(ERRORS_RESPONSE) or response.has(MESSAGE_RESPONSE)
+		login_error_label.text = "Credenciales inválidas"
+	else:
+		login_error_label.visible = true
+		login_error_label.text = "Error de conexión"
 	_enable_buttons()
 	
-	login_error_label.visible = response.has(ERRORS_RESPONSE)
+	if response_code == HTTPClient.RESPONSE_OK:
+		SceneManager.change_scene(SceneManager.SCENES.MAIN_MENU)
 
-	print(response_code, response)
 
 func _enable_buttons():
 	login_button.text = "Iniciar sesión"
 	login_button.disabled = false
 	register_button.disabled = false
-	
--- scenes/register/register.gd
+++ scenes/register/register.gd
@@ -4,13 +4,11 @@ extends Control
 @onready var email_field = $PanelContainer/VBoxContainer2/Form/Fields/EmailContainer/Email
 @onready var password_field = $PanelContainer/VBoxContainer2/Form/Fields/PasswordContainer/Password
 @onready var confirm_password_field = $PanelContainer/VBoxContainer2/Form/Fields/ConfirmPasswordContainer/ConfirmPassword
-
 @onready var error_name_label = $PanelContainer/VBoxContainer2/Form/Fields/NameContainer/ErrorLabel
 @onready var error_email_label = $PanelContainer/VBoxContainer2/Form/Fields/EmailContainer/ErrorLabel
 @onready var error_password_label = $PanelContainer/VBoxContainer2/Form/Fields/PasswordContainer/ErrorLabel
 @onready var error_confirm_password_label = $PanelContainer/VBoxContainer2/Form/Fields/ConfirmPasswordContainer/ErrorLabel
-@onready var error_http_label = $PanelContainer/VBoxContainer2/Form/Buttons/VBoxContainer/ErrorLabel
-
+@onready var error_register_label = $PanelContainer/VBoxContainer2/Form/Fields/ErrorLabel
 @onready var register_button = $PanelContainer/VBoxContainer2/Form/Buttons/VBoxContainer/Register
 @onready var login_button = $PanelContainer/VBoxContainer2/Form/Buttons/HBoxContainer/Login
 
@@ -18,7 +16,6 @@ const ERRORS_RESPONSE = "errors"
 
 var error_theme = null
 var normal_theme = null
-
 var full_name = ""
 var email = ""
 var password = ""
@@ -27,23 +24,25 @@ func _ready():
 	error_theme = load("res://scenes/register/line_edit_error.tres")
 	normal_theme = load("res://scenes/register/line_edit_normal.tres")
 
+
 func _on_register_pressed():
 	if _is_data_invalid():
 		return
 	_register()
-	
-	
+
+
 func _on_login_pressed():
 	SceneManager.change_scene(SceneManager.SCENES.LOGIN)
-	
+
+
 func _is_input_empty(text, line_edit, _errorLabel = null):
 	var is_empty = text.is_empty()
-	
 	if is_empty:
 		line_edit.add_theme_stylebox_override("normal", error_theme)
 		return is_empty
 	line_edit.add_theme_stylebox_override("normal", normal_theme)
 	return is_empty
+
 
 func _is_data_invalid():
 	full_name = name_field.text.strip_edges()
@@ -55,16 +54,17 @@ func _is_data_invalid():
 	var is_email_emtpy = _is_input_empty(email, email_field)
 	var is_password_empty = _is_input_empty(password, password_field)
 	var is_confirm_password_empty = _is_input_empty(confirm_password, confirm_password_field)
-	
 	var password_matches = _check_passwords(password, confirm_password)
 	
 	return is_name_empty or is_email_emtpy or is_password_empty or is_confirm_password_empty or password_matches
-	
+
+
 func _check_passwords(_password, _confirmPassword):
 	var password_matches = _password != _confirmPassword
 	error_confirm_password_label.visible = password_matches
 	return password_matches
-	
+
+
 func _register():
 	register_button.text = "Creando cuenta..."
 	register_button.disabled = true
@@ -86,27 +86,34 @@ func _register():
 		_enable_buttons()
 		push_error("An error occurred in the HTTP request.")
 		return
-		
+
+
 func _http_request_completed(result, response_code, headers, body):
 	var json = JSON.new()
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
+	print(response_code, response)
 	
+	if response:
+		error_register_label.visible = false
+		if response.has(ERRORS_RESPONSE):
+			_set_email_error_label(response[ERRORS_RESPONSE])
+			_set_password_error_label(response[ERRORS_RESPONSE])
+	else:
+		error_register_label.visible = true
+		error_register_label.text = "Error de conexión"
 	_enable_buttons()
-	if response.has(ERRORS_RESPONSE):
-		_set_email_error_label(response[ERRORS_RESPONSE])
-		_set_password_error_label(response[ERRORS_RESPONSE])
 
-	print(response_code)
-	print(response)
 
 func _enable_buttons():
 	register_button.text = "Registrarse"
 	register_button.disabled = false
 	login_button.disabled = false
 
+
 func _set_email_error_label(errors):		
 	error_email_label.visible = errors.has("email")
-	
+
+
 func _set_password_error_label(errors):		
 	error_password_label.visible = errors.has("password")
