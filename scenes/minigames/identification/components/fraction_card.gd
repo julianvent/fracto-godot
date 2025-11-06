84f878a2ddd15c33762167dd935514cd5a0cae30
Julian <zS22017046@estudiantes.uv.mx>
2025-11-06 09:47:03 -0600

    add: cards made droppable in slots

--- scenes/minigames/identification/components/fraction_card.gd
+++ scenes/minigames/identification/components/fraction_card.gd
@@ -2,35 +2,47 @@ extends Control
 
 var original_fraction := {"numerator": 1, "denominator": 2}
 var reduced_fraction := {"numerator": 1, "denominator": 2}
-
-var preview = null
-var original_position = Vector2.ZERO
 	
 func set_fraction(fraction):
 	original_fraction.numerator = int(fraction.numerator)
 	original_fraction.denominator = int(fraction.denominator)
-	
 	reduced_fraction = fraction.reduced
 	update_display()
 
 
-func _get_drag_data(at_position: Vector2):
-	preview = duplicate()
-	original_position = global_position
-	
-	var c = Control.new()
-	c.add_child(preview)
-	preview.position = -0.5 * preview.size
-	
-	set_drag_preview(c)
-	return self
-
 func update_display():
 	$VBoxContainer/Numerator.text = str(original_fraction.numerator)
 	$VBoxContainer/Denominator.text = str(original_fraction.denominator)
+
+
+func _get_drag_data(at_position: Vector2):
+	# DRAGGING PREVIEW
+	var preview = duplicate()
+	var c = Control.new()
+	c.add_child(preview)
+	preview.position = -0.5 * preview.size
+	set_drag_preview(c)
+	hide()
 	
-func return_to_original():
-	print(preview.global_position)
-	print(original_position)
-	get_tree().get_root().add_child(preview)
-	preview.create_tween().tween_property(preview, "position", original_position, 0.3).set_ease(Tween.EASE_OUT)
+	# DATA
+	var data = {
+		"fraction": original_fraction,
+		"reduced_fraction": reduced_fraction,
+		"card_reference": self
+	}
+	
+	return data
+
+
+func _notification(what: int):
+	if what == NOTIFICATION_DRAG_END:
+		show()
+
+
+func place_in_slot(slot_node: Node):
+	if get_parent() != slot_node:
+		get_parent().remove_child(self)
+		slot_node.add_child(self)
+		position = (slot_node.size - size) * 0.5
+	
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
--- scenes/minigames/identification/components/fraction_slot.gd
+++ scenes/minigames/identification/components/fraction_slot.gd
@@ -3,21 +3,29 @@ extends Control
 var original_fraction := {"numerator": 1, "denominator": 2}
 var reduced_fraction := {"numerator": 1, "denominator": 2}
 
-var animate = true
-
 func set_fraction(fraction):
 	original_fraction.numerator = int(fraction.numerator)
 	original_fraction.denominator = int(fraction.denominator)
-	
 	reduced_fraction = fraction.reduced
+	
 
 func _can_drop_data(at_position: Vector2, data: Variant):
-	return true
-	
+	if typeof(data) != TYPE_DICTIONARY:
+		return false
+	return data.has("fraction")
+
 func _drop_data(at_position: Vector2, data: Variant):
-	if not animate:
-		data.reparent(self)
-		data.position = (size - data.size) / 2
-	else:
-		if data.has_method("return_to_original"):
-			data.return_to_original()
+	if (typeof(data) != TYPE_DICTIONARY):
+		return
+	if not data.has("fraction") or not data.has("card_reference"):
+		return
+		
+	print(data)
+	
+	var card = data.card_reference
+	var dropped_reduced = data.reduced_fraction
+		
+	var correct = FractionUtils.fractions_equal(dropped_reduced, reduced_fraction)
+	if card and is_instance_valid(card):
+		if correct:
+			card.place_in_slot(self)
